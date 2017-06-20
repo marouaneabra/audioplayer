65009e5ca3bf3b85509ece27bb7f93221f4af589
marouaneabra <marouane.abra@gmail.com>
2017-06-19 20:10:42 -0400

    update js

---
+++ js/main.js
@@ -0,0 +1,120 @@
+var audio;
+
+$('#pause').hide();
+initAudio($('#playlist li:first-child'));
+
+function initAudio(element){
+	var song=element.attr('song');
+	var title=element.text();
+	var artist=element.attr('artist');
+
+	audio=new Audio('media/'+song);
+	if (!audio.currentTime){
+		$('#duration').html('0.00');
+	}
+
+	$('#audio-player .title').text(title);
+	$('#audio-player .artist').text(artist);
+
+	$('playlist li').removeClass('active');
+	element.addClass('active');
+};
+
+$('#play').click(function(){
+	audio.play();
+	$('#play').hide();
+	$('#pause').show();
+	$('#duration').fadeIn();
+	showDuration();
+});
+
+$('#pause').click(function(){
+	audio.pause();
+	$('#pause').hide();
+	$('#play').show();
+});
+
+$('#stop').click(function(){
+	audio.pause();
+	audio.currentTime=0;
+	$('#pause').hide();
+	$('#play').show();
+	$('#duration').fadeOut();
+});
+
+$('#next').click(function(){
+    audio.pause();
+    $('#pause').show();
+    $('#play').hide();
+    var next = $('#playlist li.active').next();
+    if (next.length == 0) {
+        next = $('#playlist li:first-child');
+    }
+    initAudio(next);
+	audio.play();
+	showDuration();
+});
+
+$('#prev').click(function(){
+    audio.pause();
+    $('#pause').show();
+    $('#play').hide();
+    var prev = $('#playlist li.active').prev();
+    if (prev.length == 0) {
+        prev = $('#playlist li:last-child');
+    }
+    initAudio(prev);
+	audio.play();
+	showDuration();
+});
+
+
+$('#playlist li').click(function () {
+    audio.pause();
+    initAudio($(this));
+	$('#play').hide();
+	$('#pause').show();
+	$('#duration').fadeIn(400);
+	audio.play();
+	showDuration();
+});
+
+$('#volume').change(function(){
+	audio.volume = parseFloat(this.value / 10);
+});
+
+function showDuration(){
+	$(audio).bind('timeupdate',function(){
+		var s=parseInt(audio.currentTime % 60);
+		var m=parseInt((audio.currentTime)/60)%60;
+		if(s<10){
+			s='0'+s;
+		}
+		$('#duration').html(m+'.'+s);
+		var value=0;
+		if (audio.currentTime>0){
+			value=Math.floor((100/audio.duration)*audio.currentTime);
+		}
+		$('#progress').css('width',value+'%');
+	});
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
